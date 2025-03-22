33c80c85500f9d32a0c8ed717d2d8023f931fd55
Fuad <fuadreddhow@gmail.com>
2025-03-23 01:43:57 +0600

    added assignment module

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -41,6 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Created apps
+    'assignment',
+
     # Installed apps
     'rest_framework',
 ]
