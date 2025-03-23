a68d73e477f7c14a9a5aed583ecd8727293c53d9
Fuad <fuadreddhow@gmail.com>
2025-03-23 21:09:01 +0600

    added assignment models to admin panel and modified views

--- backend/assignment/admin.py
+++ backend/assignment/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Assignment, AssignmentStatus
 
 # Register your models here.
+
+admin.site.register(Assignment)
+admin.site.register(AssignmentStatus)
--- backend/assignment/views.py
+++ backend/assignment/views.py
@@ -5,21 +5,23 @@ from .serializers import AssignmentSerializer, AssignmentStatusSerializer
 
 
 class AssignmentViewSet(viewsets.ModelViewSet):
+    ''' Handles creating, reading and updating assignments '''
+
+    queryset = Assignment.objects.all()
     permission_classes = (permissions.IsAuthenticated,)
     serializer_class = AssignmentSerializer
 
     def create(self, request, *args, **kwargs):
+        ''' Create a new assignment '''
         return super().create(request, *args, **kwargs)
 
-    def get_queryset(self):
-        return Assignment.objects.all()
-
 class AssignmentStatusViewSet(viewsets.ModelViewSet):
+    ''' Handles creating, reading and updating assignment statuses '''
+
+    queryset = AssignmentStatus.objects.all()
     permission_classes = (permissions.IsAuthenticated,)
     serializer_class = AssignmentStatusSerializer
 
     def create(self, request, *args, **kwargs):
+        ''' Update the status of an assignment '''
         return super().create(request, *args, **kwargs)
-
-    def get_queryset(self):
-        return AssignmentStatus.objects.all()
