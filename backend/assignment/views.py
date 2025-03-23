8900a6bd62ba88acf294a543ec0a13c09933c24c
Elaf24 <alhadi.elaf2428@gmail.com>
2025-03-23 02:58:32 -1200

    created the views.py for the assignment module/app

--- backend/assignment/views.py
+++ backend/assignment/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Assignment, AssignmentStatus
+from .serializers import AssignmentSerializer, AssignmentStatusSerializer
 
-# Create your views here.
+
+
+class AssignmentViewSet(viewsets.ModelViewSet):
+    permission_classes = (permissions.IsAuthenticated,)
+    serializer_class = AssignmentSerializer
+
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+    def get_queryset(self):
+        return Assignment.objects.all()
+
+class AssignmentStatusViewSet(viewsets.ModelViewSet):
+    permission_classes = (permissions.IsAuthenticated,)
+    serializer_class = AssignmentStatusSerializer
+
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+    def get_queryset(self):
+        return AssignmentStatus.objects.all()
