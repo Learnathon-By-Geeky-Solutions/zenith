4977af3d86eff285de72ce155ff53e5a4c1caaa5
Elaf24 <alhadi.elaf2428@gmail.com>
2025-03-26 03:30:40 +0600

    wrote the method to generate otp

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,7 +4,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework import generics
 from userauths.models import User, Profile
 from rest_framework.permissions import AllowAny
-
+import random
 # this view is for token generation
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = api_serializer.MyTokenObtainPairSerializer
@@ -16,7 +16,10 @@ class RegisterView(generics.CreateAPIView):
      queryset = User.objects.all()
      permission_classes = [AllowAny] #allows any user to use this endpoint
      serializer_class = api_serializer.RegisterSerializer
-
+#method to generate otp
+def generate_random_otp(length=7):
+    otp="".join([str(random.randint(0,9)) for _ in range(length)])
+    return otp
 
 #how to reset password
 class PasswordResetEmailVerifyAPIView(generics.RetrieveAPIView):
