e85f899d4cf46990bf21408de952256d05cb1703
Elaf24 <alhadielaf2428@gmail.com>
2025-03-25 21:06:52 +0600

    created the userauths

--- backend/userauths/admin.py
+++ backend/userauths/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from userauths.models import User, Profile
 
-# Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'full_name', 'date']
+
+#we need register our models
+admin.site.register(User)
+admin.site.register(Profile, ProfileAdmin)
+
