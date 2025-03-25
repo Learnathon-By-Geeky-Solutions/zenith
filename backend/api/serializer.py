9afefbb53b15deedb501f45ed3936231abc5d443
Elaf24 <alhadielaf2428@gmail.com>
2025-03-26 00:19:15 +0600

    created jwt token

--- backend/api/serializer.py
+++ backend/api/serializer.py
@@ -1,5 +1,15 @@
 from  rest_framework import serializers
 from  userauths.models import User, Profile
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['email'] = user.email
+        token['username'] = user.username
+        token['full_name'] = user.full_name
+        return token
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
