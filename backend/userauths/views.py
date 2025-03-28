49d4869e07798fbada63cff23c797df74425933e
Fuad <fuadreddhow@gmail.com>
2025-03-28 06:18:26 +0600

    added view for token

--- backend/userauths/views.py
+++ backend/userauths/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView
+from . import serializers as user_serializers
 
 # Create your views here.
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = user_serializers.MyTokenObtainPairSerializer
