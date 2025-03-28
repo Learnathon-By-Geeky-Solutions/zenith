1f9544b096d100524f58df6097a5681fe9b6d92e
Fuad <fuadreddhow@gmail.com>
2025-03-28 06:23:03 +0600

    added password reset view

--- backend/userauths/views.py
+++ backend/userauths/views.py
@@ -1,7 +1,9 @@
+import random
 from django.shortcuts import render
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.tokens import RefreshToken
 from . import serializers as user_serializers
 from .models import User, Profile
 
@@ -14,3 +16,24 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = [AllowAny]
     serializer_class = user_serializers.RegisterSerializer
+
+def generate_random_otp(length=7):
+    otp = ''.join(str(random.randint(0, 9)) for _ in range(length))
+    return otp
+
+class PasswordResetEmailVerifyAPIView(generics.RetrieveAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = user_serializers.UserSerializer
+
+    def get_object(self):
+        email = self.kwargs['email']
+        user = User.objects. filter(email=email).first()
+        if user:
+            uuidb64 = user.pk
+            refresh = RefreshToken.for_user(user)
+            refresh_token = str(refresh.access_token)
+            user.refresh_token = refresh_token
+            user.otp = generate_random_otp()
+            user.save()
+            link = f'http://localhost:5173/create-new-password/?otp={user.otp}&uuidb64={uuidb64}&=refresh_token{refresh_token}'
+        return user
