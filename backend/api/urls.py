0167ba47dab1c5cf97c7630e429b9ad8f5874044
Elaf24 <alhadi.elaf2428@gmail.com>
2025-03-28 19:10:02 +0600

    created the add to cart view

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,9 +2,18 @@ from django.urls import path
 from api import views as api_views
 from rest_framework_simplejwt.views import TokenRefreshView
 urlpatterns = [
+    #authentication endpoints
     path("user/token/", api_views.MyTokenObtainPairView.as_view()),
     path("register/", api_views.RegisterView.as_view()),
     path("user/token/refresh/", TokenRefreshView.as_view()),\
     path("user/password-reset/<email>/", api_views.PasswordResetEmailVerifyAPIView.as_view()),
-    path("user/password-change/", api_views.PasswordChangeAPIView.as_view())
+    path("user/password-change/", api_views.PasswordChangeAPIView.as_view()),
+
+    #course endpoints
+    path("course/category/", api_views.CategoryListAPIView.as_view()),
+    path("course/course-list/", api_views.CourseListAPIView.as_view()),
+     path("course/course-detail/<slug>", api_views.CourseDetailAPIView.as_view()),
+     
+     #adding items to cart url
+    path("course/cart/", api_views.CartAPIView.as_view()),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -6,7 +6,11 @@ from userauths.models import User, Profile
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.response import Response
+from rest_framework import status
 import random
+from api import models as api_models
+from decimal import Decimal
+import requests
 
 # this view is for token generation
 class MyTokenObtainPairView(TokenObtainPairView):
@@ -65,4 +69,88 @@ class PasswordChangeAPIView(generics.CreateAPIView):
             return Response({"message":"password changed successfully"},status=200)
         else:
             return Response({"message":"user doesn't exist"},status=400)
+
+
+class CategoryListAPIView(generics.ListAPIView):
+    queryset=api_models.Category.objects.filter(active=True)
+    serializer_class=api_serializer.CategorySerializer
+    permission_classes=[AllowAny]
+
+class CourseListAPIView(generics.ListAPIView):
+    queryset=api_models.Course.objects.filter(platform_status="Published",teacher_course_status="Published")
+    serializer_class=api_serializer.CourseSerializer
+    permission_classes=[AllowAny]
+
+class CourseDetailAPIView(generics.RetrieveAPIView):
+    serializer_class = api_serializer.CourseSerializer
+    permission_classes = [AllowAny]
+    queryset = api_models.Course.objects.filter(platform_status="Published", teacher_course_status="Published")
+
+    def get_object(self):
+        slug = self.kwargs['slug']
+        course = api_models.Course.objects.get(slug=slug, platform_status="Published", teacher_course_status="Published")
+        return course
+    
+#adding items to cart
+class CartAPIView(generics.CreateAPIView):
+    queryset = api_models.Cart.objects.all()
+    serializer_class = api_serializer.CartSerializer
+    permission_classes = [AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        course_id = request.data['course_id']  
+        user_id = request.data['user_id']
+        price = request.data['price']
+        country_name = request.data['country_name']
+        cart_id = request.data['cart_id']
+
+        print("course_id ==========", course_id)
+
+        course = api_models.Course.objects.filter(id=course_id).first()
+        
+        if user_id != "undefined":
+            user = User.objects.filter(id=user_id).first()
+        else:
+            user = None
+
+        try:
+            country_object = api_models.Country.objects.filter(name=country_name).first()
+            country = country_object.name
+        except:
+            country_object = None
+            country = "United States"
+
+        if country_object:
+            tax_rate = country_object.tax_rate / 100
+        else:
+            tax_rate = 0
+
+        cart = api_models.Cart.objects.filter(cart_id=cart_id, course=course).first()
+
+        if cart:
+            cart.course = course
+            cart.user = user
+            cart.price = price
+            cart.tax_fee = Decimal(price) * Decimal(tax_rate)
+            cart.country = country
+            cart.cart_id = cart_id
+            cart.total = Decimal(cart.price) + Decimal(cart.tax_fee)
+            cart.save()
+
+            return Response({"message": "Cart Updated Successfully"}, status=status.HTTP_200_OK)
+
+        else:
+            cart = api_models.Cart()
+
+            cart.course = course
+            cart.user = user
+            cart.price = price
+            cart.tax_fee = Decimal(price) * Decimal(tax_rate)
+            cart.country = country
+            cart.cart_id = cart_id
+            cart.total = Decimal(cart.price) + Decimal(cart.tax_fee)
+            cart.save()
+
+            return Response({"message": "Cart Created Successfully"}, status=status.HTTP_201_CREATED)
+
  
