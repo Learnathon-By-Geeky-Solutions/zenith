582eb0b150bca36fff1835b00af2b7bee7cd5742
Fuad <fuadreddhow@gmail.com>
2025-03-28 06:21:14 +0600

    added registration serializers and view

--- backend/userauths/views.py
+++ backend/userauths/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
 from . import serializers as user_serializers
+from .models import User, Profile
 
 # Create your views here.
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = user_serializers.MyTokenObtainPairSerializer
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [AllowAny]
+    serializer_class = user_serializers.RegisterSerializer
