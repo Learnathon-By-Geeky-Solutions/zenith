11a3293c42324bf04c155cfe3ec0ebf5606c5ba2
Elaf24 <alhadi.elaf2428@gmail.com>
2025-04-12 09:39:22 +0600

    StudentCourseDetailAPIView

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -40,4 +40,7 @@ path("cart/stats/<cart_id>/", api_views.CartStatsAPIView.as_view()),
 
 #course list api view
 path("student/course-list/<user_id>/", api_views.StudentCourseListAPIView.as_view()),
+
+#course detail api view
+path("student/course-detail/<user_id>/<enrollment_id>/", api_views.StudentCourseDetailAPIView.as_view()),
 ]
